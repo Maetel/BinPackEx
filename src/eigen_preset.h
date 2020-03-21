6d06332ca7c6b4f754aa1a23a5679e3c1bea5e84
welcometothegalaxy <iamjam4944@gmail.com>
2020-03-21 21:18:11 +0900

    add link eigen

---
+++ src/eigen_preset.h
@@ -0,0 +1,28 @@
+// eigen_preset.h
+
+#include <Eigen/Dense>
+
+#define DefMatrix(num, postfix) using Matrix##num##postfix = Eigen::Matrix##num##postfix
+#define DefVector(num, postfix) using Vector##num##postfix = Eigen::Vector##num##postfix
+
+//using Vector3f = Eigen::Vector3f; ... 
+DefVector(2, f);
+DefVector(3, f);
+DefVector(4, f);
+DefVector(2, d);
+DefVector(3, d);
+DefVector(4, d);
+
+//using Matrix3f = Eigen::Vector3f; ... 
+DefMatrix(2, f);
+DefMatrix(3, f);
+DefMatrix(4, f);
+DefMatrix(2, d);
+DefMatrix(3, d);
+DefMatrix(4, d);
+
+using VectorRGB = Eigen::Matrix<unsigned char, 3, 1>;
+using Vector3u = VectorRGB;
+using VectorRGBA = Eigen::Matrix<unsigned char, 4, 1>;
+using Vector4u = VectorRGBA;
+
