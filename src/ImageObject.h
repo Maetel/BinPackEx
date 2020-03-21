7e0adb4dea8cc273ec46547d80befd187ea32bda
welcometothegalaxy <iamjam4944@gmail.com>
2020-03-21 21:18:29 +0900

    add image object / image object example

---
+++ src/ImageObject.h
@@ -0,0 +1,392 @@
+// ImageObject.h
+
+#pragma once
+
+#include <QImage>
+#include <QString>
+#include "eigen_preset.h"
+
+
+
+
+class ImageObject
+{
+public:
+	virtual int pixelCount() const = 0;
+	virtual int length() const = 0; //same as pixelCount
+	virtual int pixelSize() const = 0;
+
+	//data size in bytes
+	virtual int dataSize() const = 0;
+	virtual int width() const = 0;
+	virtual int height() const = 0;
+	virtual unsigned char* bits() = 0;
+	virtual unsigned char const* const bits() const = 0;
+	virtual void clear() = 0;
+	virtual void resize(int wid, int hi) = 0;
+	virtual bool empty() const = 0;
+	
+	virtual bool isStandardImageType() const = 0;
+	virtual bool isGrayType() const = 0;
+	virtual bool isRGBType() const = 0;
+	virtual bool isRGBAType() const = 0;
+
+public:
+	virtual ~ImageObject() {}
+};
+using ImageObjectPtr = std::shared_ptr<ImageObject>;
+
+template <typename T>
+inline T QRgbAllocator(QRgb val)
+{
+#define IS_T(_type) std::is_same<T, _type>::value
+
+	//check supported types
+	static_assert(
+		IS_T(unsigned char) ||
+		IS_T(VectorRGB) ||
+		IS_T(VectorRGBA)
+		);
+
+	if constexpr (IS_T(unsigned char))
+		return (T)qGray(val);
+	
+	if constexpr (IS_T(VectorRGB))
+		return VectorRGB{ (unsigned char)qRed(val), (unsigned char)qGreen(val), (unsigned char)qBlue(val) };
+	
+	if constexpr (IS_T(VectorRGBA))
+		return VectorRGB{ (unsigned char)qRed(val), (unsigned char)qGreen(val), (unsigned char)qBlue(val), (unsigned char)qAlpha(val) };
+}
+
+template <typename T>
+class ImageData : public ImageObject
+{
+public:
+	using PixelType = T;
+
+	ImageData() { clear(); }
+	ImageData(QString path) { clear();  this->load(path); }
+	ImageData(int width, int height, unsigned char* data = nullptr)
+	{
+		clear();
+		_alloc(width, height);
+		if (data)
+			memcpy(m_data, data, dataSize());
+	}
+	ImageData(ImageData const& rhs)
+	{
+		*this = rhs;
+	}
+	ImageData(ImageData&& rhs)
+	{
+		clear();
+		m_wid = rhs.m_wid;
+		m_hi = rhs.m_hi;
+		std::swap(m_data, rhs.m_data);
+	}
+	ImageData& operator=(ImageData const& rhs)
+	{
+		clear();
+		_alloc(rhs.m_wid, rhs.m_hi, rhs.m_data);
+		return *this;
+	}
+
+	~ImageData() { clear(); }
+
+#pragma region Overrides
+	// Overrides
+
+	//type definition field
+	template<typename T2>
+	constexpr bool pixelTypeIs() const { return std::is_same<T, T2>::value; };
+#define IS_GRAY_IMAGE (pixelTypeIs<unsigned char>())
+#define IS_RGB_IMAGE (pixelTypeIs<VectorRGB>())
+#define IS_RGBA_IMAGE (pixelTypeIs<VectorRGBA>())
+#define GRAY_IMAGE_Q_FORM QImage::Format_Grayscale8
+#define RGB24_IMAGE_Q_FORM QImage::Format_RGB888
+#define RGB32_IMAGE_Q_FORM QImage::Format_RGB32
+#define RGBA_IMAGE_Q_FORM QImage::Format_RGB32
+
+	bool isStandardImageType() const override { return isGrayType() || isRGBType() || isRGBAType(); }
+	bool isGrayType() const override { return IS_GRAY_IMAGE; }
+	bool isRGBType() const override { return IS_RGB_IMAGE; }
+	bool isRGBAType() const override { return IS_RGBA_IMAGE; }
+	// !type definition field
+
+	unsigned char* bits() override { return reinterpret_cast<unsigned char*>(m_data); }
+	unsigned char const* const bits() const override { return reinterpret_cast<unsigned char*>(m_data); }
+	int pixelCount() const override { return m_wid * m_hi; }
+	int length() const override { return pixelCount(); }
+	int pixelSize() const override { return sizeof(T); }
+	//data size in bytes	
+	int dataSize() const override { return sizeof(T) * m_wid * m_hi; }
+	int width() const override { return m_wid; }
+	int height() const override { return m_hi; };
+	void clear() override { _Delete();  m_wid = default_wid; m_hi = default_hi; };
+	void resize(int wid, int hi) override { _alloc(wid, hi); };
+	bool empty() const override { return m_data == nullptr; };
+	
+	// ! Overrides
+#pragma endregion
+	
+#pragma region ImageIO
+	// Image IOs
+	void fromQImage(QImage const& input)
+	{
+		auto format = input.format();
+		int wid = input.width(), hi = input.height();
+
+		const auto this_form = toQImageFormat();
+
+		if (this_form == QImage::Format_Invalid)
+		{
+			throw std::logic_error("QImage Format invalid");
+		}
+
+		const bool isSameFormat = this_form == format;
+		QImage buf = input;
+		if (isSameFormat)
+		{
+			this->_alloc(buf.width(), buf.height(), buf.constBits());
+		}
+		else
+		{
+			//buf = buf.convertToFormat(this_form);
+
+			const int wid = buf.width(), hi = buf.height();
+			std::vector<T> dataBuf(wid * hi);
+
+			for (int y = 0; y < hi; ++y)
+				for (int x = 0; x < wid; ++x)
+					dataBuf.at(x + y * wid) = QRgbAllocator<T>(buf.pixel(x, y));
+
+			this->_alloc(wid, hi, dataBuf.data());
+		}
+	}
+
+	QImage toQImage() const
+	{
+		QImage retval;
+
+		auto this_form = toQImageFormat();
+		if (!m_data || this_form == QImage::Format_Invalid)
+			return retval;
+
+		return QImage(bits(), m_wid, m_hi, this_form);
+	}
+
+	QImage::Format toQImageFormat() const
+	{
+		return
+			IS_GRAY_IMAGE ? GRAY_IMAGE_Q_FORM :
+			IS_RGB_IMAGE ? RGB24_IMAGE_Q_FORM :
+			IS_RGBA_IMAGE ? RGB32_IMAGE_Q_FORM :
+			QImage::Format_Invalid
+			;
+	}
+
+	bool load(QString path)
+	{
+		const auto this_form = toQImageFormat();
+
+		if (this_form == QImage::Format_Invalid)
+			return false;
+
+		QImage buf;
+		if (!buf.load(path))
+			return false;
+
+		fromQImage(buf);
+
+		return true;
+	}
+	bool save(QString path) const
+	{
+		return toQImage().save(path);
+	}
+	// ! Image IOs
+#pragma endregion
+	
+#pragma region Data_Access
+	// data accessors
+	T& operator()(int idx) { return m_data[idx]; }
+	T const& operator()(int idx) const { return m_data[idx]; }
+	T& operator()(int x, int y) { return m_data[x + y * m_wid]; }
+	T const& operator()(int x, int y) const { return m_data[x + y * m_wid]; }
+	T& at(int idx) { return m_data[idx]; }
+	T const& at(int idx) const { return m_data[idx]; }
+	T& at(int x, int y) { return m_data[x + y * m_wid]; }
+	T const& at(int x, int y) const { return m_data[x + y * m_wid]; }
+
+	T* data() { return m_data; }
+	T const* const data() const { return m_data; }
+
+	std::vector<T> dataVector() const
+	{
+		if (!m_data)
+			return std::vector<T>();
+
+		std::vector<T> retval(this->pixelCount());
+		memcpy(retval.data(), m_data, this->dataSize());
+		return retval;
+	}
+	
+	template<typename FuncT>
+	void for_each_px(bool parallel, FuncT&& func)
+	{
+		if (parallel)
+			_for_each_px_parallel(std::forward<FuncT>(func));
+		else
+			_for_each_px_serial(std::forward<FuncT>(func));
+	}
+
+	template<typename FuncT>
+	void for_each_idx(bool parallel, FuncT&& func)
+	{
+		if (parallel)
+			_for_each_idx_parallel(std::forward<FuncT>(func));
+		else
+			_for_each_idx_serial(std::forward<FuncT>(func));
+	}
+	// ! data accessors
+#pragma endregion
+
+	void fill(T val)
+	{
+		std::fill(m_data, m_data + pixelCount(), val);
+	}
+	void set(T val) { fill(std::forward<T>(val)); }
+
+	//Converter = lambdaFunction(InputType value) {
+	// //do something with value...
+	// return (OutputType) retval;
+	//}
+	template <typename OutT, typename Converter>
+	ImageData<OutT> convert(Converter&& function) const
+	{
+		ImageData<OutT> retval(m_wid, m_hi);
+
+		for (int idx = 0; idx < pixelCount(); ++idx)
+			retval(idx) = function(m_data[idx]);
+		
+		return retval;
+	}
+
+	template <typename OutT>
+	ImageData<OutT> convert() const
+	{
+		ImageData<OutT> retval(m_wid, m_hi);
+
+		for (int idx = 0; idx < pixelCount(); ++idx)
+			retval(idx) = static_cast<OutT>(m_data[idx]);
+		
+		return retval;
+	}
+
+protected:
+	void _Delete()
+	{
+		if (m_data)
+			delete m_data;
+		m_data = nullptr;
+	}
+	template<typename T2 = T>
+	void _alloc(int wid, int hi, T2 const* data = nullptr)
+	{
+		clear();
+		m_wid = wid;
+		m_hi = hi;
+		m_data = new T[(size_t)wid * hi];
+
+		if (data)
+			memcpy(m_data, reinterpret_cast<unsigned char const* const>(data), dataSize());
+	}
+
+	template<typename FuncT>
+	void _for_each_px_parallel(FuncT&& func)
+	{
+		throw std::logic_error("parallel for each px not implemented @" __FUNCTION__);
+	}
+
+	template<typename FuncT>
+	void _for_each_px_serial(FuncT&& func)
+	{
+		for (int y = 0; y < m_hi; ++y)
+			for (int x = 0; x < m_wid; ++x)
+				func(x, y, (*this)(x, y));
+	}
+
+	template<typename FuncT>
+	void _for_each_idx_parallel(FuncT&& func)
+	{
+		throw std::logic_error("parallel for each idx not implemented @" __FUNCTION__);
+	}
+
+	template<typename FuncT>
+	void _for_each_idx_serial(FuncT&& func)
+	{
+		for (int idx = 0; idx < pixelCount(); ++idx)
+			func(idx, *this(idx));
+	}
+
+protected:
+	const int default_wid = 0, default_hi = 0;
+	int m_wid, m_hi;
+	T* m_data = 0;
+};
+
+#define DECL_PTR(x) using x##Ptr = std::shared_ptr<x>
+
+using ImageData8 = ImageData<unsigned char>;
+using GrayImage = ImageData8;
+using ImageData16 = ImageData<short>;
+using ImageDataFloat = ImageData<float>;
+using ImageDataDouble = ImageData<double>;
+using ImageDataRGB = ImageData<VectorRGB>;
+using RGBImage = ImageData<VectorRGB>;
+using ImageDataRGBA = ImageData<VectorRGBA>;
+using RGBAImage = ImageData<VectorRGBA>;
+
+DECL_PTR(ImageData8);
+DECL_PTR(ImageDataFloat);
+DECL_PTR(ImageDataDouble);
+DECL_PTR(ImageDataRGB);
+DECL_PTR(ImageDataRGBA);
+
+#undef DECL_PTR
+
+#pragma region Inline functions
+
+template<typename T = void>
+inline unsigned char RGB2Gray(VectorRGB val)
+{
+	return
+		static_cast<unsigned char>(
+			double{ val[0] * 0.2126 } +
+			double{ val[1] * 0.7152 } +
+			double{ val[2] * 0.0722 }
+	);
+}
+
+template<typename T = void>
+inline ImageData8 RGB2Gray(ImageDataRGB const& image)
+{
+	return image.convert<unsigned char>([](VectorRGB rgbVal)
+		{
+			return RGB2Gray(rgbVal);
+		});
+}
+
+//this method doesn't handle under/overflow
+template<typename TOut, typename TIn>
+constexpr TOut saturateCast(TIn val)
+{
+	constexpr auto maxVal = std::numeric_limits<TOut>::max();
+	constexpr auto minVal = std::numeric_limits<TOut>::min();
+	return
+		(val > maxVal) ? maxVal :
+		(val < minVal) ? minVal :
+		val;
+}
+
+#pragma endregion
---
+++ src/ImageObjectExample.h
@@ -0,0 +1,100 @@
+// ImageObjectExample.h
+
+#include "ImageObject.h"
+#include <QDebug>
+
+template <typename T = void>
+class _HereIs
+{
+public:
+	_HereIs(QString welcomeMsg) : msg(welcomeMsg) { qDebug() << '\n' << "==========" << welcomeMsg << "==========" ; }
+	~_HereIs() { qDebug() << "==========" << "Finished [" << msg <<"]" << "==========" << '\n'; }
+
+	QString msg;
+};
+#define HereIs(x) _HereIs __hereisinstance(QString(#x))
+
+template<typename T = void>
+class ImageObjectExample
+{
+public:
+	ImageObjectExample() { run(); }
+
+	void run()
+	{
+		QString dirpath = "C:/work/cpp/BinPackEx/resources/images/";
+		QString someRGB = QString("%1%2").arg(dirpath).arg("img (1).jpg");
+		QString toGray = QString("%1%2").arg(dirpath).arg("img (1)-gray.jpg");
+		QString grayAsRGB = QString("%1%2").arg(dirpath).arg("img (1)-gray-rgb.jpg");
+
+		ImageData8Ptr grayImage = std::make_shared<ImageData8>();
+		grayImage->load(someRGB); //if RGB image loaded into a gray image format, it automatically converts to gray type
+		grayImage->save(toGray);
+
+		ImageDataRGBPtr rgbImage = std::make_shared<ImageDataRGB>(toGray); //load gray-fied image as RGB type
+		rgbImage->save(grayAsRGB);
+
+
+		{
+			HereIs(show basic infos);
+
+			qDebug() << QString("Is image loaded : %1, is image rgb type : %2, is image gray type : %3")
+				.arg(!rgbImage->empty())
+				.arg(rgbImage->isRGBType())
+				.arg(rgbImage->isGrayType());
+			qDebug() << QString("Width = %1 height = %2, pixelCount = %3")
+				.arg(rgbImage->width())
+				.arg(rgbImage->height())
+				.arg(rgbImage->pixelCount());
+			qDebug() << QString("Pixel size in bytes : %1, Total data size in bytes : %2")
+				.arg(rgbImage->pixelSize())
+				.arg(rgbImage->dataSize());
+		}
+
+		{
+			HereIs(using base class methods);
+
+			std::vector<ImageObjectPtr> images
+			{
+				grayImage,
+				rgbImage
+			};
+
+			for (auto const& img : images)
+			{
+				std::cout << "Is this image gray type ? : " << img->isGrayType() << std::endl;
+			}
+		}
+		
+		{
+			HereIs(converting images to other types);
+
+			ImageDataRGB someRGBImage(someRGB);
+			someRGBImage.save("beforeCasted.bmp");
+			ImageData8 dst = someRGBImage.convert<unsigned char>([](VectorRGB rgbVal)
+				{
+					return RGB2Gray(rgbVal);
+				});
+			dst.save("castedToGray.bmp");
+
+			ImageData16 dst16 = dst.convert<short>();
+		}
+
+		
+		{
+			HereIs(manipulating each pixel);
+
+			ImageData8 grayImage(someRGB);
+			ImageData8 brighterGrayImage = grayImage;
+
+			bool computeParallel = false; // parallel is not implemented yet
+			brighterGrayImage.for_each_px(computeParallel, [](int x, int y, auto& px)
+				{
+					px = saturateCast<unsigned char, int>(px + 50); //make brighter
+				});
+
+			grayImage.save("someGrayImg.bmp");
+			brighterGrayImage.save("someBrighterGrayImg.bmp");
+		}
+	}
+};
